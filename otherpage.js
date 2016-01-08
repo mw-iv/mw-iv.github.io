b53576922cfe28adcea54c9f0b78761abe43f427
mw-iv <binkyiv@yahoo.com>
2016-01-08 12:25:18 -0600

    update to extra page

---
+++ otherpage.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	
+	$('#vol').animate({width: '13.5em'}, 1000);
+	
+/*----- Volunteer animation ------*/
+  $('#vol').click(function(){
+	$(".hsWordsE").hide();
+	$(".hsWordsV").fadeIn('slow');
+	$("#colBox0").css("opacity", "0.0");
+	$("#colBox1").css("opacity", "0.0");
+	$("#colBox2").css("opacity", "0.0");
+	$("#colBox3").css("opacity", "0.0");
+	
+	$("#exAct").hide();
+	$("#volWo").show();
+	$("#vol").hide();
+	$("#ex").show();
+	
+	$("#vol").css("width", "14.5em");
+	$('#ex').animate({width: '14.5em'},1000);
+  });
+  
+ /*----- Ex. Activities animation ------*/
+  $('#ex').click(function(){
+	$(".hsWordsV").hide();
+	$(".hsWordsE").fadeIn('slow');
+	$("#colBox0").css("opacity", "1.0");
+	$("#colBox1").css("opacity", "1.0");
+	$("#colBox2").css("opacity", "1.0");
+	$("#colBox3").css("opacity", "1.0");
+	
+	$("#volWo").hide();
+	$("#exAct").show();
+	$("#ex").hide();
+	$("#vol").show();
+	
+	$("#ex").css("width", "12em");
+	$('#vol').animate({width: '12em'},1000);
+  });
+
+});
